541b86dcff886240f6e42ddc5dfcf9dea18ede4f
RicoWD <pascual.machio.eric@gmail.com>
2024-10-25 03:16:14 +0200

    c04 ex02

--- Practice/Exercices/c04/ex02/ft_putnbr.c
+++ Practice/Exercices/c04/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: codephenix5 <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 09:24:54 by codephenix5       #+#    #+#             */
-/*   Updated: 2024/10/24 11:58:23 by codephenix5      ###   ########.fr       */
+/*   Updated: 2024/10/25 03:06:52 by ep               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 void	ft_putnbr(int nb)
 {
 	char	nb_c;
-
-	if (nb>=10)
+	
+	if (nb < 0)
 	{
-		nb_c = (nb%10)+48;
-
-		nb = (nb/10);
+		write (1, "-", 1);
+		nb = -nb;
 	}
-	if (nb<10)
-	{
-		nb_c = (nb%10)+48;
-		write(1, &nb_c, 1);
-		nb = (nb/10);
-	}
+	if (nb >= 10)
+        ft_putnbr(nb / 10);
+	nb_c = (nb % 10) + 48;
+	write (1, &nb_c, 1);
 }
 
 #include <stdio.h>
@@ -35,5 +32,12 @@ void	ft_putnbr(int nb)
 int	main(void)
 {
 	ft_putnbr(21);
+	write(1, "\n", 1);
+	ft_putnbr(0);
+	write(1, "\n", 1);
+	ft_putnbr(713);
+	write(1, "\n", 1);
+	ft_putnbr(-1);
+	write(1, "\n", 1);
 	return (0);
 }
