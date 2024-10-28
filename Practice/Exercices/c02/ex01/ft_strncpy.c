a357d68393e9960ee9abe93b7764c2a17b8fe96b
RicoWD <pascual.machio.eric@gmail.com>
2024-10-28 03:04:05 +0100

    c02 ex02

--- Practice/Exercices/c02/ex01/ft_strncpy.c
+++ Practice/Exercices/c02/ex01/ft_strncpy.c
@@ -6,14 +6,22 @@
 /*   By: codephenix5 <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:27:16 by codephenix5       #+#    #+#             */
-/*   Updated: 2024/10/23 12:58:44 by codephenix5      ###   ########.fr       */
+/*   Updated: 2024/10/28 03:02:48 by ep               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int	i;
 
-}*/
+	i=0;
+	while(src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
 
 #include <string.h>
 #include <stdio.h>
