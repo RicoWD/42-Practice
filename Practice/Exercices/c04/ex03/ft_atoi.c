199e316c13855b1eae582962bea7a1fc81849ad4
RicoWD <pascual.machio.eric@gmail.com>
2024-11-28 23:49:13 +0100

    c04 ex04

--- Practice/Exercices/c04/ex03/ft_atoi.c
+++ Practice/Exercices/c04/ex03/ft_atoi.c
@@ -6,18 +6,41 @@
 /*   By: codephenix5 <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:51:09 by codephenix5       #+#    #+#             */
-/*   Updated: 2024/11/19 09:59:35 by codephenix5      ###   ########.fr       */
+/*   Updated: 2024/11/28 23:46:53 by ep               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(char *str)
 {
-	
+	int	i;
+	int	j;
+	int	sign_counter;
+	int	nbr;
+
+	i = 0;
+	j = 0;
+	sign_counter = 0;
+	nbr = 0;
+	if ((str[i]) && (str[i] == ' ') || (str[i] >= '0') || (str[i] <= '9'))
+	{
+		if (str[i] == '-')
+			sign_counter++;
+		if (sign_counter % 2 == 0)
+			nbr = '-';
+			j++;
+		if (str[i] >= '0' && str[i] <= '9')
+			nbr = str[i];
+			j++;
+		i++;
+	}
+	return (nbr);
 }
+
+#include <stdio.h>
 
 int	main(void)
 {
-	char	str[20] = "Hello, je ne sais pas encore ce que je dois faire";
+	char	str[20] = "   -+--+1234o1223";
 
 	printf("Voilà, ce que je reçois %d\n", ft_atoi(str));
 }
