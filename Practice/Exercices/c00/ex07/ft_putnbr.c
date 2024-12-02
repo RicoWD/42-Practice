06602ec797d31213a88f3f653a0968137049ac76
Eric <pascual.machio.eric@gmail.com>
2024-12-02 16:58:39 +0100

    c04 ex03 ex04

---
+++ Practice/Exercices/c00/ex07/ft_putnbr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codephenix5 <marvin@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 12:24:49 by codephenix5       #+#    #+#             */
+/*   Updated: 2024/12/02 15:15:20 by codephenix5      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c_nb)
+{
+	write (1, &c_nb, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb == 0)
+	{
+		ft_putchar('0');
+		return;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + '0');
+}
+
+int	main(void)
+{
+	int	nb;
+
+	nb = 42;
+	ft_putnbr(nb);
+	ft_putchar('\n');
+}
--- Practice/Exercices/c04/ex03/ft_atoi.c
+++ Practice/Exercices/c04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: codephenix5 <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:51:09 by codephenix5       #+#    #+#             */
-/*   Updated: 2024/11/29 13:01:37 by codephenix5      ###   ########.fr       */
+/*   Updated: 2024/12/02 11:50:31 by codephenix5      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,28 @@ int	ft_atoi(char *str)
 	i = 0;
 	sign_counter = 0;
 	nbr = 0;
-	while ((str[i]) && ((str[i] == ' ') || (str[i] >= '0') || (str[i] <= '9')))
+
+	while (str[i] == ' ')
+		i++;
+
+	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			sign_counter++;
-		if (sign_counter % 2 == 0)
-		{
-			nbr = '-';
-		}
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			nbr = nbr * 10 + str[i];
-		}
+		i++;
+	}	
+	
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - 48);
 		i++;
 	}
+
+	if (sign_counter % 2 != 0)
+	{
+		nbr = -nbr;
+	}
+
 	return (nbr);
 }
 
@@ -40,15 +48,20 @@ int	ft_atoi(char *str)
 
 int	main(void)
 {
+	char	str42[20] = "   ---+--+1234ab567"; 
 	char	str0[20] = "12343";
 	char	str1[20] = "1234o1223";
 	char	str2[20] = "-1234o1223";
 	char	str3[20] = "--+1234o1223";
 	char	str4[20] = "   -+--+1234o1223";
+	char	str5[20] = "   -+--+12 34o1223";
 
+	printf("Rendu 42 %d\n", ft_atoi(str42));
 	printf("Rendu 0 %d\n", ft_atoi(str0));
 	printf("Rendu 1 %d\n", ft_atoi(str1));
 	printf("Rendu 2 %d\n", ft_atoi(str2));
 	printf("Rendu 3 %d\n", ft_atoi(str3));
 	printf("Rendu 4 %d\n", ft_atoi(str4));
+	printf("Rendu 5 %d\n", ft_atoi(str5));
+
 }
