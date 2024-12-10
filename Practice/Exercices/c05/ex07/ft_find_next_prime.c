2c296f8dbc786bf6afd76ee74c10b55a7ee741c2
Eric <pascual.machio.eric@gmail.com>
2024-12-10 13:08:26 +0100

    c05 ex07

--- Practice/Exercices/c05/ex07/ft_find_next_prime.c
+++ Practice/Exercices/c05/ex07/ft_find_next_prime.c
@@ -6,19 +6,45 @@
 /*   By: codephenix5 <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 16:49:14 by codephenix5       #+#    #+#             */
-/*   Updated: 2024/12/09 16:54:57 by codephenix5      ###   ########.fr       */
+/*   Updated: 2024/12/10 12:57:39 by codephenix5      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int is_prime(int nb)
+{
+    int i;
+
+    i = 2;
+    if (nb <=1)
+        return (0);
+    if (nb == 2)
+        return (1);
+    while (i < nb)
+    {
+        if (nb % i == 0)
+        {
+            return (0);
+        }
+        i++;
+    }
+    return (1);
+}
+
 int	ft_find_next_prime(int nb)
 {
-	
+	while (!is_prime(nb))
+	{
+		nb++;
+	}
+	return nb;
 }
+
+#include <stdio.h>
 
 int	main(void)
 {
 	int	nb;
 
-	nb = 7;
+	nb = 15;
 	printf("The next prime number is... %d\n", ft_find_next_prime(nb));
 }
