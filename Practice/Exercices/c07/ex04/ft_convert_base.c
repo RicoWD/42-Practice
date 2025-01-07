e6f9414bd7640470a6fcfa7d38381bf0f7ab665e
Eric <pascual.machio.eric@gmail.com>
2025-01-07 13:03:34 +0100

    c07 ex04

--- Practice/Exercices/c07/ex04/ft_convert_base.c
+++ Practice/Exercices/c07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: ep <marvin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 13:00:44 by ep                #+#    #+#             */
-/*   Updated: 2025/01/06 16:57:09 by ep               ###   ########.fr       */
+/*   Updated: 2025/01/07 13:02:23 by codephenix5      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	is_base_ok(char *base)
 		while (b < len)
 		{
 			if (base[a] == base[b])
-				return(0)
+				return(0);
 			b++;
 		}
 		a++;
@@ -48,14 +48,14 @@ int	get_base_index(char str, char *base)
 	while (base[k])
 	{
 		if (str == base[k])
-			return[k];
+			return (k);
 		k++;
 	}
 	return (-1);
 }
 
 
-int	ft_atoi_base(*str, char *base)
+int	ft_atoi_base(char *str, char *base)
 {
 	int	i;
 	int	j;
@@ -93,14 +93,68 @@ int	ft_atoi_base(*str, char *base)
 	return (result);
 }
 
-
-char	*ft_convert_base(char *nbr, char *base_from, char base_to)
+char	*int_to_str(int	nbr_int, char *base)
 {
-	if (!is_base_ok(base_from) || !is_back_ok(base_to))
+	char	*nbr_char;
+	int		base_len;
+	int		malloc_need;
+	int		count_nbr_int;
+	int		empty_char;
+
+	base_len = 0;
+	malloc_need = 0;
+	count_nbr_int = nbr_int;
+	empty_char = 0;
+	// VÉRIFICATION
+	if (nbr_int == 0)
+	{
+		char	*zero_str = (char *)malloc(2); 
+		if (!zero_str)
+			return (NULL);
+		zero_str[0] = '0';
+		zero_str[1] = '\0';
+		return (zero_str);
+	}
+
+	// DÉCOMPTES
+	while (base[base_len])
+		base_len++;
+	if (nbr_int < 0)
+		malloc_need++;
+	while (count_nbr_int != 0)
+	{
+		count_nbr_int = count_nbr_int / base_len;
+		malloc_need++;
+	}
+	nbr_char = (char *)malloc(malloc_need + 1);
+
+	// TRANSFORMATIONS
+	if (nbr_int < 0)
+	{
+		nbr_char[0] = '-';
+		nbr_int = -nbr_int;
+	}
+	while (nbr_int != 0)
+	{
+		nbr_int = nbr_int % base_len;
+		nbr_char[malloc_need] = base[nbr_int];
+		malloc_need--;
+		empty_char++;
+	}
+	nbr_char[empty_char] = '\0'; 
+	return (nbr_char);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int		nbr_from;
+	char	*nbr_to;
+
+	if (!is_base_ok(base_from) || !is_base_ok(base_to))
 		return NULL;
-
-	return (nbr);
-
+	nbr_from = ft_atoi_base(nbr, base_from);
+	nbr_to = int_to_str(nbr_from, base_to);
+	return (nbr_to);
 }
 
 
@@ -116,5 +170,5 @@ int	main(void)
 	base_from = "0123456789";
 	base_to = "01";
 
-	printf("Résultat : %s\n", ft_convert_base(nbr, base_from, base_to);
+	printf("Résultat : %s\n", ft_convert_base(nbr, base_from, base_to));
 }
